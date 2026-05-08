da3f3e8ccd02685ae19d94b79b87a51b53766503
Kapil Gaire <eriaglipak312@gmail.com>
2026-05-08 12:29:35 +0545

    added loading to / page

--- app/company/dashboard/page.js
+++ app/company/dashboard/page.js
@@ -1,8 +1,10 @@
 import getDashboardData from "@/lib/utils/company/getDashboardData";
 import TopSection from "./components/TopSection";
 
-export default async function Dashboard() {
-  await getDashboardData();
+export default async function Dashboard({ searchParams }) {
+  const { filter } = await searchParams;
+  const dashboardData = await getDashboardData(filter);
+  console.log(dashboardData);
   return (
     <div>
       <TopSection />
