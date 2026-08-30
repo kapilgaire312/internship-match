98903ed6f9d1c0e3796619288d4e68685f398993
Kapil Gaire <eriaglipak312@gmail.com>
2026-08-30 23:32:49 +0545

    fix: bug in tailwind classes not being applied properly.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 export default {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
